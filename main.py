f8321fc0a0e1f37dd63a605b07659b54c26d6ed3
vladimir-mihailov <you@example.com>
2024-12-14 11:03:24 +0400

    Практическое задание по уроку "Базовые структуры данных"_ Вводный модуль

---
+++ main.py
@@ -0,0 +1,32 @@
+# 1st program
+
+print(9**0.5*5)
+
+
+# 2nd program
+
+print(9.99>9.98, 1000 != 100.1)
+
+
+# 3rd program
+
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+
+#4th program
+
+"123.456"
+print(4)
+a = float("123.456")
+print(a)
+print(a*10)
+print(a**10)
+print(a/10)
+print(a//10)
+print(a%10)
+print(int(a*10))
+print(int(a/10))
+
+
